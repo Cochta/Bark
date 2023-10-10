29683bf409ad67d1940c96d9e934997b06662245
Coch <constantin.verine@gmail.com>
2023-10-10 16:41:31 +0200

    Euler finished

--- engineTests/eulerTests.cpp
+++ engineTests/eulerTests.cpp
@@ -3,69 +3,152 @@
 
 TEST(Euler, OneIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(1), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(1), e, MathUtility::Epsilon);
 }
 TEST(Euler, TwoIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(2), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(2), e, MathUtility::Epsilon);
 }
 TEST(Euler, ThreeIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(3), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(3), e, MathUtility::Epsilon);
 }
 TEST(Euler, FourIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(4), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(4), e, MathUtility::Epsilon);
 }
 TEST(Euler, FiveIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(5), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(5), e, MathUtility::Epsilon);
 }
 TEST(Euler, SixIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(6), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(6), e, MathUtility::Epsilon);
 }
 TEST(Euler, SevenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(7), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(7), e, MathUtility::Epsilon);
 }
 TEST(Euler, EightIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(8), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(8), e, MathUtility::Epsilon);
 }
 TEST(Euler, NineIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(9), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(9), e, MathUtility::Epsilon);
 }
 TEST(Euler, TenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(10), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(10), e, MathUtility::Epsilon);
 }
 TEST(Euler, ElevenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(11), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(11), e, MathUtility::Epsilon);
 }
 TEST(Euler, TwelveIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(12), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(12), e, MathUtility::Epsilon);
 }
 TEST(Euler, ThirteenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(14), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(14), e, MathUtility::Epsilon);
 }
 TEST(Euler, FourteenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(15), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(15), e, MathUtility::Epsilon);
 }
 TEST(Euler, FifteenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(16), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(16), e, MathUtility::Epsilon);
 }
 TEST(Euler, SixteenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(17), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(17), e, MathUtility::Epsilon);
 }
 TEST(Euler, SeventeenIteration)
 {
-    ASSERT_NEAR(GetEulerNumber(18), e, Epsilon);
+    ASSERT_NEAR(GetEulerNumber(18), e, MathUtility::Epsilon);
+}
+TEST(Euler, EighteenIteration)
+{
+ASSERT_NEAR(GetEulerNumber(19), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, NineteenIteration)
+{
+ASSERT_NEAR(GetEulerNumber(20), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyIteration)
+{
+ASSERT_NEAR(GetEulerNumber(21), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyOneIteration)
+{
+ASSERT_NEAR(GetEulerNumber(22), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyTwoIteration)
+{
+ASSERT_NEAR(GetEulerNumber(23), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyThreeIteration)
+{
+ASSERT_NEAR(GetEulerNumber(24), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyFourIteration)
+{
+ASSERT_NEAR(GetEulerNumber(25), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyFiveIteration)
+{
+ASSERT_NEAR(GetEulerNumber(26), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentySixIteration)
+{
+ASSERT_NEAR(GetEulerNumber(27), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentySevenIteration)
+{
+ASSERT_NEAR(GetEulerNumber(28), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyEightIteration)
+{
+ASSERT_NEAR(GetEulerNumber(29), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, TwentyNineIteration)
+{
+ASSERT_NEAR(GetEulerNumber(30), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, ThirtyIteration)
+{
+ASSERT_NEAR(GetEulerNumber(31), e, MathUtility::Epsilon);
+}
+TEST(Euler, ThirtyOneIteration)
+{
+ASSERT_NEAR(GetEulerNumber(32), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, ThirtyTwoIteration)
+{
+ASSERT_NEAR(GetEulerNumber(33), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, ThirtyThreeIteration)
+{
+ASSERT_NEAR(GetEulerNumber(34), e, MathUtility::Epsilon);
+}
+
+TEST(Euler, ThirtyFourIteration)
+{
+ASSERT_NEAR(GetEulerNumber(35), e, MathUtility::Epsilon);
 }
--- include/euler.h
+++ include/euler.h
@@ -2,36 +2,15 @@
 
 #include <utility>
 #include <vector>
+#include "../libs/Math922/include/MathUtility.h"
 
-constexpr static float Epsilon = 0.00001f;
-const double e =  2.71828;
-
-template <typename T>
-[[nodiscard]] constexpr T Abs(T nbr) noexcept
-{
-    return nbr < 0 ? -nbr : nbr;
-}
-
-template<typename T>
-[[nodiscard]] constexpr T Fact(T n) noexcept {
-    T result = 1;
-
-    for (int i = 2; i <= Abs(n); i++) {
-        result *= i;
-    }
-
-    if (n < 0) {
-        result = -result;
-    }
-
-    return result;
-}
+const double e =  2.718281828459045; // 15 decimales
 
 [[nodiscard]] constexpr double GetEulerNumber(int taylorIter) noexcept {
     double result = 0.f;
 
     for (int i = 0; i < taylorIter; ++i) {
-        result += 1.0f / (double)Fact(i);
+        result += 1.0f / (double)MathUtility::Fact((double)i);
     }
 
     return result;
